9c58ad44dd4fced89ba282e94a4731504dbcb7cf
Juan Carlos <rnbm2025@gmail.com>
2025-10-15 05:32:10 -0500

    Update render-build.sh
    VA

--- whatsapp_bot/render-build.sh
+++ whatsapp_bot/render-build.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y chromium fonts-liberation libappindicator3-1 xdg-utils
+apt-get install -y chromium \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libnss3 \
+  libxss1 \
+  libxshmfence1 \
+  libatk-bridge2.0-0 \
+  libgbm1 \
+  libxdamage1 \
+  xdg-utils \
+  wget \
+  unzip
